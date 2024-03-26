b59da4cd70681b9a035fc8a708d3fffede693c23
62hoon99 <ske06008@gmail.com>
2024-03-26 21:25:26 +0900

    fix: cors origin 설정 추가

--- src/main/java/com/cstapin/auth/config/SecurityConfig.java
+++ src/main/java/com/cstapin/auth/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
-        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+        configuration.addAllowedOrigin("https://cs-tap-in.site"); // cs-tap-in.site 도메인만 허용
         configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
         configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
